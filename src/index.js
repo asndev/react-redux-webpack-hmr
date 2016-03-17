a31703eee01c08d5f8de455062546a8e9b625cec
Anton Stepan <info@antonstepan.it>
2016-03-17 19:13:20 +0100

    stylesheets. base.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 
+import 'stylesheets/_base.scss';
+
 const store = configureStore();
 const rootEl = document.getElementById('root');
 
